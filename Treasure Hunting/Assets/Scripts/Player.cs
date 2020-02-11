82d015632cc0d7561bb00613b482562de0a61d5f
Natalia DaLomba <nataliadalomba@gmail.com>
2020-02-10 21:52:57 -0500

    Movement started

---
+++ Treasure Hunting/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody r;
+
+    void Start() {
+		r = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float jump = Input.GetAxis("Jump");
+
+		Vector3 movement = new Vector3(0, jump, moveHorizontal);
+
+		r.AddForce(movement * speed);
+    }
+}
